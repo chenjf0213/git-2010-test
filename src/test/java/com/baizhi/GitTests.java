de5702c4b962a2c0b842ef82b2d84bedf538f1ef
chenjf <2546883045@qq.com>
2021-06-06 21:41:32 +0800

    添加功能

---
+++ src/test/java/com/baizhi/GitTests.java
@@ -0,0 +1,12 @@
+package com.baizhi;
+
+import org.junit.Test;
+
+public class GitTests {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("-----");
+        int a=20;
+        int b=30;
+    }
+}
